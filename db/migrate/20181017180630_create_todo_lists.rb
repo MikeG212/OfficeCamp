e16c56e335a6b743b7fa1de6ec13d85d2822b9c3
MikeG212 <michaelcgee93@gmail.com>
2018-10-17 11:08:49 -0700

    Style login form and add todos frontend

---
+++ db/migrate/20181017180630_create_todo_lists.rb
@@ -0,0 +1,6 @@
+class CreateTodoLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todo_lists do |t|
+    end
+  end
+end
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -15,19 +15,20 @@ import {
 import SignupContainer from './session/signup_container';
 import HomeContainer from './home/home_container';
 import LoginContainer from './session/login_container';
+import TodoListContainer from './todo_list/todo_list_container';
 
 const App = () => (
   <div>
     <header>
     </header>
-
     <Switch>
       <AuthRoute exact path="/login" component={LoginContainer} />
       <AuthRoute exact path="/signup" component={SignupContainer} />
+
       <Route exact path="/" component={HomeContainer} />
       <Redirect to="/" />
     </Switch>
   </div>
-)
+);
 
-export default App
+export default App;
